8ad6fc54f444ce125f8be3528529fe82ee0564e4
Datsteelfn2 <Datsteelfn@gmail.com>
2025-01-07 21:36:04 -0500

    Finally added a way to check each word, didnt need a for loop just a simple if statement did the trick

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ import random
 
 
 word="randomlol"
-
+picked_letters=[]
 #listed_word=list(word)
 print(word)
 counter=len(word)
 #what we can do is first ask user input of a letter in this "word", then iterate through that word to see if the user 
 # is matching their guesses with the letters
-for letter in range(len(word)):
-    guess=input("Guess a letter in a random word:> ")
+while True:
+    guess=input("Guess a letter in this word:> ")
+    
     if guess in word:
-        counter-=1
-        print(f"Correct,{letter} is a part of the word, only {counter} letters left")
+       
+        if guess in picked_letters:
+            print("Youve already picked that letter")
+        elif guess not in picked_letters:
+            print("Correct that letter is in the word")
+        picked_letters.append(guess)
     elif guess not in word:
-        print(f"Not quite,{counter} guesses left")
+        print("Incorrect guess")
