eefcda43d7001cd4ebbf786ad328a99a65f1716a
Datsteelfn2 <Datsteelfn@gmail.com>
2025-01-07 21:24:51 -0500

    updated version, still kind of works

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import random 
-list_of_words=["united","senegal","pizza","impossible","unlucky"]
-lives=10
+#word=random.choice(list_of_words)
 
-word=random.choice(list_of_words)
-listed_word=list(word)
+
+word="randomlol"
+
+#listed_word=list(word)
 print(word)
+counter=len(word)
 #what we can do is first ask user input of a letter in this "word", then iterate through that word to see if the user 
 # is matching their guesses with the letters
-for letter in listed_word:
+for letter in range(len(word)):
     guess=input("Guess a letter in a random word:> ")
-    if guess==letter:
-        print(f"Correct,{letter} is a part of the word")
+    if guess in word:
+        counter-=1
+        print(f"Correct,{letter} is a part of the word, only {counter} letters left")
+    elif guess not in word:
+        print(f"Not quite,{counter} guesses left")
