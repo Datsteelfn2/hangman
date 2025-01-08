ac8ff9ec1cb535db5332839a1dcf8f27239f9ff8
Datsteelfn2 <Datsteelfn@gmail.com>
2025-01-07 21:44:59 -0500

    tested a coutner variable, just need it to detect duplicates

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ print(word)
 counter=len(word)
 #what we can do is first ask user input of a letter in this "word", then iterate through that word to see if the user 
 # is matching their guesses with the letters
+print(counter)
 while True:
     guess=input("Guess a letter in this word:> ")
     
     if guess in word:
-       
+        counter-=1
         if guess in picked_letters:
             print("Youve already picked that letter")
         elif guess not in picked_letters:
-            print("Correct that letter is in the word")
+            print(f"Correct that letter is in the word, only {counter} letters left")
         picked_letters.append(guess)
     elif guess not in word:
         print("Incorrect guess")
